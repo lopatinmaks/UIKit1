ba5ad61a751589b2d2b857b1e7058a2d51c44e42
lopatinmaks <lopatin.maks2611@gmail.com>
2022-10-06 11:58:46 +0400

    Initial Commit

---
+++ UIKit1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit1
+//
+//  Created by Ольга on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
