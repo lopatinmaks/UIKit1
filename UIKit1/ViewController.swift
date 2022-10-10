915ee03e35388030495300e1b62ad456ec4b01f7
lopatinmaks <lopatin.maks2611@gmail.com>
2022-10-10 15:25:15 +0400

    HomeWork1.2

--- UIKit1/ViewController.swift
+++ UIKit1/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var phraseLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func actionButton(_ sender: Any) {
+        welcome(title: "Привет", message: "Введите слово")
+    }
+    func welcome(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "Ввод"
+            }
+        let action = UIAlertAction(title: "ok", style: .default) { action in
+            self.phraseLabel.text = BrilliantApplication.brilliantApp.correctPhrase(alertController.textFields?.first?.text ?? "")
+        }
 
-}
+                alertController.addAction(action)
+                self.present(alertController, animated: true, completion: nil)
+            }
+            struct BrilliantApplication {
+                public static let brilliantApp = BrilliantApplication()
+
+                func correctPhrase(_ phrase : String) -> String {
+                    if phrase == "leohl" {
+                        return "hello"
+                    } else {
+                        return "Данные не были получены"
+                    }
+                }
+            }
+        }
+
 
