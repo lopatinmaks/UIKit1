a2932ad477581c6849acf3f11857aaaa3d614b93
lopatinmaks <lopatin.maks2611@gmail.com>
2022-10-17 17:58:28 +0400

    HomeWorkAudoi

---
+++ UIKit1/InfoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InfoViewController.swift
+//  UIKit1
+//
+//  Created by Ольга on 15.10.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+}
+
+
+
